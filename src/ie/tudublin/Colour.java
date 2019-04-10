23879db652f2db63f3df907b939ac1267fb3c447
Martyna <martyna.nieckarz98@gmail.com>
2019-04-10 17:05:11 +0100

    made more methods

--- src/ie/tudublin/Colour.java
+++ src/ie/tudublin/Colour.java
@@ -1,11 +1,19 @@
 package ie.tudublin;
 
-import processing.data.Table;
-
 public class Colour {
 
 
-        }
-    }
 
+    // constructor
+
+//    public (String number, double balance, String custName,
+//                   String custEmailAdd, String custPhoneNo) {
+//        System.out.println("Account constructor with parameters called");
+//        this.number = number;
+//        this.balance = balance;
+//        this.custEmailAdd = custEmailAdd;
+//        this.custName = custName;
+//        this.custPhoneNo = custPhoneNo;
+//
+//
 }
--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -3,6 +3,7 @@ package ie.tudublin;
 import processing.core.PApplet;
 import processing.data.Table;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class UI extends PApplet {
@@ -39,16 +40,44 @@ public class UI extends PApplet {
 		private ArrayList<String> colours = new ArrayList<>();
 	}
 
+	// resistors array list
+	public class resistors {
+		private ArrayList<String> resistors = new ArrayList<>();
+	}
+
+	// function for loading in the table
 	public void loadColours() {
 		//
 		Table table = loadTable("colours.csv", "header");
 
 		for (int i = 0; i < table.getRowCount(); i++) {
-			// creates a new star
-			Star s = new Star(table.getRow(i));
+			Colour colour = new String(table.getRow(i));
 
 			// adds the star to the end of the stars array list
-			stars.add(s);
+			colour.add(s);
+
+			println(table.getRowCount() + " total rows in table");
+
+			String colour = row.getString("colours");
+			int r = row.getInt("r");
+			int g = row.getInt("g");
+			int b = row.getInt("b");
+			String value = row.getString("value");
+
+			println(colour + " has an RGB value of " + r + g + b + value);
 		}
+	}
+
+	// method for printing the contents of the array
+	public void printColours {
+		System.out.println("you have " + colours.size() + " items in your list");
+		for(int i=0; i<colours.size(); i++) {
+			System.out.println((i+1) + ". " + colours.get(i));
+		}
+	}
+
+	// method for returning a colour associated with value parameter
+	public Color findColor(int value) {
+		
 	}
 }
