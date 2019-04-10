8b7ab17f68ba17685bafbe50d08448ce6e6096ed
Martyna <martyna.nieckarz98@gmail.com>
2019-04-10 17:36:08 +0100

    changed lots

--- src/ie/tudublin/Colour.java
+++ src/ie/tudublin/Colour.java
@@ -1,19 +1,58 @@
 package ie.tudublin;
 
 public class Colour {
+
     private String colour;
     public int r;
     public int g;
     public int b;
     public int value;
+    public int TableRow;
+
+    // table for colours
+    public Colour(TableRow row) {
+        colour = row.getString("Colour");
+        r = row.getInt("r");
+        g = row.getFloat("g");
+        b = row.getFloat("b");
+        value = row.getFloat("value");
+    }
 
 
     // constructor
-    public Colour (String colour, int r, int g , int b, int value){
+    public Colour(String colour, int r, int g , int b, int value){
         this.colour = colour;
         this.r = r;
         this.g = g;
         this.b = b;
         this.value = value;
+    }
+
+    // Getters
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    //toStrong method
+    public String toString()
+    {
+        return colour + "\t" + r + "\t" + g + "\t" + b + "\t" + value;
     }
 }
--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -1,9 +1,7 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
-import processing.data.Table;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 public class UI extends PApplet {
@@ -12,7 +10,7 @@ public class UI extends PApplet {
 
 		int hundreds = (value / 100);
 		int tens = (value - (hundreds * 100)) / 10;
-		int ones = value - ((hundreds * 100)  + (tens * 10));
+		int ones = value - ((hundreds * 100) + (tens * 10));
 		print(hundreds + ",");
 		print(tens + ",");
 		println(ones);
@@ -21,18 +19,16 @@ public class UI extends PApplet {
 	public void settings() {
 
 		size(500, 800);
-		
+
 		separate(381);
 		separate(1);
 		separate(92);
 	}
 
-	public void setup() 
-	{
+	public void setup() {
 	}
-	
-	public void draw()
-	{			
+
+	public void draw() {
 	}
 
 	// this is my array list of colours
@@ -45,39 +41,54 @@ public class UI extends PApplet {
 		private ArrayList<String> resistors = new ArrayList<>();
 	}
 
-	// function for loading in the table
-	public void loadColours() {
-		//
-		Table table = loadTable("colours.csv", "header");
+//	// function for loading in the table
+//	public void loadColours() {
+//		//
+//		Table table = loadTable("colours.csv", "header");
+//
+//		for (int i = 0; i < table.getRowCount(); i++) {
+//			//Colour colour = new String(table.getRow(i));
+//
+//			//println(table.getRowCount() + " total rows in table");
+//
+//			String colour = i.getString("colours");
+//			int r = i.getInt("r");
+//			int g = i.getInt("g");
+//			int b = i.getInt("b");
+//			String value = i.getString("value");
+//
+//			System.out.println(colour + " has an RGB value of " + r + g + b + value);
+//		}
+//	}
 
-		for (int i = 0; i < table.getRowCount(); i++) {
-			Colour colour = new String(table.getRow(i));
 
-			// adds the star to the end of the stars array list
-			colour.add(s);
+//	public void loadColours() {
+//		Table table = loadTable("colours.csv", "header");
+//
+//		for (int i = 0; i < table.getRowCount(); i++) {
+//			Colour s = new Colour(table.getRow(i));
+//
+//			Colour.add(s);
+//		}
+//	}
 
-			println(table.getRowCount() + " total rows in table");
 
-			String colour = row.getString("colours");
-			int r = row.getInt("r");
-			int g = row.getInt("g");
-			int b = row.getInt("b");
-			String value = row.getString("value");
+//	// method for printing the contents of the array
+//	public void printColours() {
+//		System.out.println("you have " + colours.size() + " items in your list");
+//		for (int i = 0; i < colours.size(); i++) {
+//			System.out.println((i + 1) + ". " + colours.get(i));
+//		}
+//	}
 
-			println(colour + " has an RGB value of " + r + g + b + value);
-		}
-	}
-
-	// method for printing the contents of the array
-	public void printColours {
-		System.out.println("you have " + colours.size() + " items in your list");
-		for(int i=0; i<colours.size(); i++) {
-			System.out.println((i+1) + ". " + colours.get(i));
-		}
-	}
-
-	// method for returning a colour associated with value parameter
-	public Color findColor(int value) {
-
-	}
+//	// method for returning a colour associated with value parameter
+//	public Color findColor(int value) {
+//		if(colours.contains(colour))
+//	}
+//
+//	if(girls.contains(nameSearch)) {
+//		// Look for the data
+//	} else {
+//		// print error
+//	}
 }
